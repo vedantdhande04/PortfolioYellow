e62dbf709bd19ad05ba3c3d0444f17182f0d9203
vedantdhande04 <76739690+vedantdhande04@users.noreply.github.com>
2024-03-29 17:12:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -23,13 +23,59 @@ $( "#logo" ).hover(
      }
  );
 
+//for circle 1
+
  $( ".fast" ).hover(
   function() {
-    $( this ).addClass( "fastanimated" );},
+    $( this ).addClass( "fastanimated" );   
+    $(".circle1").addClass("longtransform");
+  }
+        ,
     function(){
       $( this ).removeClass( "fastanimated" );
+      $(".circle1").removeClass("longtransform");
     });
 
+    $(".fast").click(function() {
+      $(".circle1").toggleClass("longanime");
+      $(".circle1").addClass("longtransform");
+  });
+
+//for circle 2
+
+  $( ".evolve" ).hover(
+    function() {
+      $( this ).addClass( "fastanimated" );   
+      $(".circle2").addClass("longtransform");
+    }
+          ,
+      function(){
+        $( this ).removeClass( "fastanimated" );
+        $(".circle2").removeClass("longtransform");
+      });
+  
+      $(".evolve").click(function() {
+        $(".circle2").toggleClass("longanime");
+        $(".circle2").addClass("longtransform");
+    });
+
+//for circle 3
+
+    $( ".dynamic" ).hover(
+      function() {
+        $( this ).addClass( "fastanimated" );   
+        $(".circle3").addClass("longtransform");
+      }
+            ,
+        function(){
+          $( this ).removeClass( "fastanimated" );
+          $(".circle3").removeClass("longtransform");
+        });
+    
+        $(".dynamic").click(function() {
+          $(".circle3").toggleClass("longanime");
+          $(".circle3").addClass("longtransform");
+      });
 
     $( ".evolve" ).hover(
       function() {
