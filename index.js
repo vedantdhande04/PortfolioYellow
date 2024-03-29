83ec1ce25d455ca094eac1d9fd6c5234a4a3e79f
vedantdhande04 <76739690+vedantdhande04@users.noreply.github.com>
2024-03-29 14:02:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -15,8 +15,42 @@ $( "#logo" ).hover(
     function() {
      $( this ).removeClass( "circleanime" );
      $( this ).addClass( "circleanimated" );
+     
    },function() {
        $( this ).addClass( "circleanime" );
        $( this ).removeClass( "circleanimated" );
+       
      }
  );
+
+ $( ".fast" ).hover(
+  function() {
+    $( this ).addClass( "fastanimated" );},
+    function(){
+      $( this ).removeClass( "fastanimated" );
+    });
+
+
+    $( ".evolve" ).hover(
+      function() {
+        $( this ).addClass( "fastanimated" );},
+        function(){
+          $( this ).removeClass( "fastanimated" );
+        });
+    
+        $( ".dynamic" ).hover(
+          function() {
+            $( this ).addClass( "fastanimated" );},
+            function(){
+              $( this ).removeClass( "fastanimated" );
+            });
+        
+  
+            $( ".nav-link" ).hover(
+              function() {
+                $( this ).addClass( "navanimate" );},
+                function(){
+                  $( this ).removeClass( "navanimate" );
+                });
+
+                
